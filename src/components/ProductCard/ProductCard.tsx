ed516d18b72493d3bcbcd5d751774031580c1889
Saajid Muhammad <saajithtm@gmail.com>
2022-03-04 14:09:12 +0530

    style fixed

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -53,42 +53,38 @@ const ProductCard: FC = () => {
     <>
 
 
-      <div>
-      </div>
-      <Row gutter={[32, 32]} justify="center">
+      <div className="cards-wrapper__ProductCard ">
 
         {isLoaded ? allProducts?.map((prod, i) => {
 
           return (
-            <Col span={5} key={i}>
-              <Card
-                hoverable
-                cover={<img alt="example" src="https://www.notebookcheck.net/fileadmin/_processed_/0/9/csm_Untitled_2_5_2a2aef7903.jpg" style={{ height: 280 }} />}
+            <Card
+              hoverable
+              className='card__ProductCard'
+              cover={<img alt="example" src="https://www.notebookcheck.net/fileadmin/_processed_/0/9/csm_Untitled_2_5_2a2aef7903.jpg" style={{ height: 280 }} />}
+            >
 
-              >
+              <Link to="/product-details/123">
 
-                <Link to="/product-details/123">
+                <div className='product-name__ProductCard'>
+                  Name
+                </div>
 
-                  <div className='product-name__ProductCard'>
-                    Name
-                  </div>
+                <div className='price__ProductCard'>
+                  12000 LKR
+                </div>
 
-                  <div className='price__ProductCard'>
-                    12000 LKR
-                  </div>
+                <div>
+                  <Rate disabled defaultValue={2} />
+                </div>
+              </Link>
 
-                  <div>
-                    <Rate disabled defaultValue={2} />
-                  </div>
-                </Link>
-
-                <Button type="primary" block style={{ marginTop: "10px" }} >Add To Cart</Button>
-
-
-              </Card>
+              <Button type="primary" block style={{ marginTop: "10px" }} >Add To Cart</Button>
 
 
-            </Col>
+            </Card>
+
+
 
           )
 
@@ -97,7 +93,7 @@ const ProductCard: FC = () => {
 
 
 
-      </Row>
+      </div>
 
     </>
 
