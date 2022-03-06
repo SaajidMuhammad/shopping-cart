ac9f7c57c4ccb2b1b85cc6c84b057c301bfe6b82
Saajid Muhammad <saajithtm@gmail.com>
2022-03-06 20:15:11 +0530

    detail page logics implemented

--- src/components/DetailedProduct/DetailedProduct.tsx
+++ src/components/DetailedProduct/DetailedProduct.tsx
@@ -15,6 +15,7 @@ const DetailedProduct: FC = () => {
   // States
   const [currProd, setCurrProd] = useState<any | null>(null)
   const [prodRefetched, setProdRefetched] = useState<boolean>(false)
+  const [quantity, setQuantity] = useState<number>(1)
 
   useEffect(() => {
     let currentId = window.location.href.split("/").pop()
@@ -54,58 +55,61 @@ const DetailedProduct: FC = () => {
   return (
     <div className='wrapper__DetailedProduct'>
       <Card>
-        <div className='container__DetailedProduct'>
-          <div className='image-div__DetailedProduct'>
-            <Image
-              className='image__DetailedProduct'
-              src="https://www.notebookcheck.net/fileadmin/_processed_/0/9/csm_Untitled_2_5_2a2aef7903.jpg"
-            />
-          </div>
-          <div className='details__DetailedProduct'>
-            <div className='title__DetailedProduct'>
-              Product Name  Product Name  Product Name  Product Name  Product Name
-            </div>
 
-            <div className='rating__DetailedProduct'>
-              <Rate disabled defaultValue={2} />
-              <div className='rating-no__DetailedProduct'>
-                23 Ratings
+        {currProd != null &&
+          <div className='container__DetailedProduct'>
+            <div className='image-div__DetailedProduct'>
+              <Image
+                className='image__DetailedProduct'
+                src={currProd[0].imgURL}
+              />
+            </div>
+            <div className='details__DetailedProduct'>
+              <div className='title__DetailedProduct'>
+                {currProd[0].name}
+              </div>
+
+              <div className='rating__DetailedProduct'>
+                <Rate disabled defaultValue={2} />
+                <div className='rating-no__DetailedProduct'>
+                  {currProd[0].totalRates}
+                </div>
+              </div>
+
+              <Divider />
+
+
+              <div className='description__DetailedProduct'>
+                {currProd[0].description}
+              </div>
+
+              <Divider />
+
+
+              <div className='price__DetailedProduct'>
+                {currProd[0].price + " " + currProd[0].currency}
+              </div>
+
+              <Divider />
+
+              <div className='quantity-wrapper__DetailedProduct'>
+                <Button icon={<MinusOutlined />} onClick={() => { quantity > 1 ? setQuantity(quantity - 1) : setQuantity(quantity) }} />
+
+                <div className='quantity__DetailedProduct'>
+                  {quantity}
+                </div>
+                <Button icon={<PlusOutlined onClick={() => { quantity < 10 ? setQuantity(quantity + 1) : setQuantity(quantity) }} />} />
+
+              </div>
+
+              <div className='button-wrapper__DetailedProduct' >
+                <Button type="primary" block >
+                  ADD TO CART
+                </Button>
               </div>
             </div>
+          </div>}
 
-            <Divider />
-
-
-            <div className='description__DetailedProduct'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-            </div>
-
-            <Divider />
-
-
-            <div className='price__DetailedProduct'>
-              27000 LKR
-            </div>
-
-            <Divider />
-
-            <div className='quantity-wrapper__DetailedProduct'>
-              <Button icon={<MinusOutlined />} />
-
-              <div className='quantity__DetailedProduct'>
-                2
-              </div>
-              <Button icon={<PlusOutlined />} />
-
-            </div>
-
-            <div className='button-wrapper__DetailedProduct' >
-              <Button type="primary" block >
-                ADD TO CART
-              </Button>
-            </div>
-          </div>
-        </div>
       </Card>
     </div>
   )
